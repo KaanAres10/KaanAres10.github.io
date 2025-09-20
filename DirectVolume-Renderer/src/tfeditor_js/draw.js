c777e3cf1ec19367173b3e138827082ddcbf4fb3
Kaan Altıntaş <k.altiintas@student.tudelft.nl>
2025-09-20 02:25:19 +0200

    Page

--- DirectVolume-Renderer/src/tfeditor_js/draw.js
+++
@@ -1,9 +0,0 @@
-export function drawControlPoint(context, x, y, pointSize) {
-    const strokes = 10;
-    for (let i = 0; i < strokes; i++) {
-        context.beginPath();
-        context.strokeStyle = i % 2 === 0 ? 'white' : 'black';
-        context.arc(x, y, pointSize, (i / strokes) * (2 * Math.PI), ((i + 1) / strokes) * (2 * Math.PI));
-        context.stroke();
-    }
-}
--- DirectVolume-Renderer/src/tfeditor_js/index.js
+++
@@ -1,6 +0,0 @@
-export { TransferFunctionEditor } from './TransferFunctionEditor';
-export { ColorMapEditor } from './ColorMapEditor';
-export { ColorPicker } from './ColorPicker';
-export { TransparencyEditor } from './TransparencyEditor';
-export * from './convert';
-export * from './Types';
